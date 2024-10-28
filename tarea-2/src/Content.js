e753f6e9a922eee3d20baeb4d457c79bb11ab7ac
ElMaikina <mikesoto890@gmail.com>
2024-10-28 19:38:49 -0300

    visual changes

--- tarea-2/src/Content.js
+++ tarea-2/src/Content.js
@@ -4,7 +4,6 @@ function Content() {
   return (
     <div className="Content">
       <h1>Bienvenido!</h1>
-
       React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library[4][5] for building user interfaces based on components by Facebook Inc. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies.[6][7][8]
       React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality.[9][10] A key advantage of React is that it only rerenders those parts of the page that have changed, avoiding unnecessary re-rendering of unchanged DOM elements. It was first launched on 29 May 2013.[11] 
     </div>
