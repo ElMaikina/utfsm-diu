4cf437bd7a6d5b2d154b39bd2c06425c4ad8f9a0
Martin Pino Cornejo <martin.pino@sansano.usm.cl>
2024-11-04 03:32:38 -0300

    feat(mockData.js): add mock data for simulation purposes

---
+++ tarea-2/src/assets/mockData.js
@@ -0,0 +1,28 @@
+export const candidatesBySubject = {
+    'Arquitectura de Computadores': [
+      { id: 1, name: 'Leo Pardo', status: 'previous-assistant', statusText: 'Ayudante del semestre anterior' },
+      { id: 2, name: 'Tigre Sano', status: 'meets-requirements', statusText: 'Cumple los requisitos' },
+      { id: 3, name: 'Zorro Loro', status: 'other-application', statusText: 'Postulando en otra ayudantía' }
+    ],
+    'Sistemas Operativos': [
+      { id: 4, name: 'Gato Rito', status: 'previous-assistant', statusText: 'Ayudante del semestre anterior' },
+      { id: 5, name: 'Rana Tona', status: 'meets-requirements', statusText: 'Cumple los requisitos' },
+      { id: 6, name: 'Perro Dido', status: 'other-application', statusText: 'Postulando en otra ayudantía' }
+    ],
+    'Redes de Computadoras': [
+      { id: 7, name: 'Lince Listo', status: 'previous-assistant', statusText: 'Ayudante del semestre anterior' },
+      { id: 8, name: 'Pato Nato', status: 'meets-requirements', statusText: 'Cumple los requisitos' },
+      { id: 9, name: 'Conejo Rápido', status: 'other-application', statusText: 'Postulando en otra ayudantía' }
+    ],
+    'Bases de Datos': [
+      { id: 10, name: 'Mono Sabe', status: 'previous-assistant', statusText: 'Ayudante del semestre anterior' },
+      { id: 11, name: 'Oso Sito', status: 'meets-requirements', statusText: 'Cumple los requisitos' },
+      { id: 12, name: 'Cabra Brinca', status: 'other-application', statusText: 'Postulando en otra ayudantía' }
+    ],
+    'Desarrollo Web': [
+      { id: 13, name: 'Lobo Tino', status: 'previous-assistant', statusText: 'Ayudante del semestre anterior' },
+      { id: 14, name: 'Puma Nino', status: 'meets-requirements', statusText: 'Cumple los requisitos' },
+      { id: 15, name: 'Zebra Lena', status: 'other-application', statusText: 'Postulando en otra ayudantía' }
+    ]
+  };
+  
