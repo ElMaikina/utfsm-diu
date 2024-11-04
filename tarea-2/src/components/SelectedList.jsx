dd6d02a2ab80b61686fe0cdef16222580f399c97
Martin Pino Cornejo <martin.pino@sansano.usm.cl>
2024-11-04 03:40:46 -0300

    feat(SelectedList.jsx): create component for displaying selected items

---
+++ tarea-2/src/components/SelectedList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../stylesheets/Candidates.css';
+
+function SelectedList({ seleccionados, onRemove }) {
+  return (
+    <div className="candidates__column candidates__column--seleccionados">
+      <h2 className="candidates__title">Seleccionados</h2>
+      <ul className="candidates__list">
+        {seleccionados.map((seleccionado) => (
+          <li key={seleccionado.id} className="candidates__item">
+            <div className="candidates__info">
+              <span className="candidates__name">{seleccionado.name}</span>
+            </div>
+            <button 
+              className="candidates__button candidates__button--remove"
+              onClick={() => onRemove(seleccionado.id)}
+            >
+              Quitar
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SelectedList;
