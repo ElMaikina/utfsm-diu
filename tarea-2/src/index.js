c0565e4953cadf5e7266b4911e445327af53241b
Martin Pino Cornejo <martin.pino@sansano.usm.cl>
2024-11-13 22:05:32 -0300

    Update index.js

--- tarea-2/src/index.js
+++ tarea-2/src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-
-import NavBar from './components/navbar';
-import Content from './components/content';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import { UserProvider } from './contexts/UserContext';
+import { SelectedAssistantsProvider } from './contexts/SelectedAssistantsContext'; // Importa el proveedor
 import './stylesheets/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <NavBar />
-    <Content />
+    <UserProvider>
+      <SelectedAssistantsProvider> {/* Envuelve la aplicación */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<Signin />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </Router>
+      </SelectedAssistantsProvider>
+    </UserProvider>
   </React.StrictMode>
 );
-
