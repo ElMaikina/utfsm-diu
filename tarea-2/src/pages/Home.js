4ddeaf547c35275801d12eb5af2bc6f2835f25dd
Martin Pino Cornejo <martin.pino@sansano.usm.cl>
2024-11-13 22:05:34 -0300

    Create Home.js

---
+++ tarea-2/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/navbar';
+import Content from '../components/content';
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+      <Content />
+    </div>
+  );
+}
+
+export default Home;
