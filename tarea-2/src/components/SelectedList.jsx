4adc5fa28c2650220f31c216e5062d375c64bcc2
Martin Pino Cornejo <martin.pino@sansano.usm.cl>
2024-11-13 22:05:23 -0300

    Update SelectedList.jsx

--- tarea-2/src/components/SelectedList.jsx
+++ tarea-2/src/components/SelectedList.jsx
@@ -4,16 +4,16 @@ import '../stylesheets/Finalizar.css';
 
 function SelectedList({ seleccionados, onRemove, onMoreInfo }) {
   return (
-    <div className="candidates__column candidates__column--seleccionados">
+    <div className="candidates__column--seleccionados">
       <h2 className="candidates__title">Seleccionados</h2>
-      <ul className="candidates__list">
+      <ul className=" candidates__list--selected">
         {seleccionados.map((seleccionado) => (
-          <li key={seleccionado.id} className="candidates__item">
+          <li key={seleccionado.id} className="candidates__item--selected">
             <div className="candidates__info">
               <span className="candidates__name">{seleccionado.name}</span>
             </div>
             <div className="candidates__buttons">
-            <button 
+              <button 
                 className="candidates__button candidates__button--more-info candidates__button--discrete"
                 onClick={() => onMoreInfo(seleccionado.id)}
               >
@@ -25,7 +25,6 @@ function SelectedList({ seleccionados, onRemove, onMoreInfo }) {
               >
                 Quitar
               </button>
-              
             </div>
           </li>
         ))}
